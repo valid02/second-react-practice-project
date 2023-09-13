71a60e0979712740f7d28f151620950c0460fdd5
Valid Rabiee <validrabieejs@gmail.com>
2023-09-13 18:17:59 +0330

    adding two way binding in form input

--- src/components/ToDoInpot/ToDoInput.js
+++ src/components/ToDoInpot/ToDoInput.js
@@ -12,13 +12,14 @@ const ToDoInput = (props) => {
     event.preventDefault();
 
     props.onAddTodo(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="todo-input">
         <label>To Do</label>
-        <input type="text" onChange={todoInputChangeHandler} />
+        <input type="text" value={enteredValue} onChange={todoInputChangeHandler} />
       </div>
       <button type='submit' className="todo-input-btn">Add Todo</button>
     </form>
